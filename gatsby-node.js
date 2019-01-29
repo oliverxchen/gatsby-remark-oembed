06e5d82c3c973c94661c26c1660f7a27ea44624f
oliverxchen <oliverxchen@users.noreply.github.com>
2019-01-29 18:49:13 +0800

    logging

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,13 +6,21 @@ const {
 } = require("./helpers");
 
 exports.onPreBootstrap = async ({ cache }, rawOptions) => {
+  console.log('ONPREBOOTSTRAP');
   const options = ammendOptions(rawOptions);
   const rawProviders = await fetchOembedProviders();
+  console.log('rawProviders');
+  console.log(rawProviders);
   const providers = processProviders(rawProviders, options);
+  console.log('providers');
+  console.log(providers);
   await cache.set("remark-oembed-providers", providers);
 };
 
 const processProviders = (providers, options) => {
+  console.log('PROCESSPROVIDERS');
   providers = ammendProviders(providers);
+  console.log('providers');
+  console.log(providers);
   return filterProviders(providers, options.providers);
 };
